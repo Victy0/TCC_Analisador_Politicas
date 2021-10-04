b6594a68efd560d1c656e02287aeabb2ff381660
Victor Marques <victyrmarques@gmail.com>
2021-10-04 20:12:25 -0300

    Criação da estrutura de dados para gerenciamento de análises de políticas de privacidade

--- core/steps/requestUrl.py
+++ core/steps/requestUrl.py
@@ -6,17 +6,16 @@ from rest_framework.response import Response
 
 from pdfminer import high_level
 import requests
-from datetime import datetime
+
 #
 #  Extrai texto de arquivos 
 #
-def textExtractor (request):
+def textExtractor (request, fileId):
     # Obtem os 4 ultimos caracteres do url
     exten = request[-4:]
     #Verifica o tipo de arquivo que está sendo provido pela url e decide qual metodo usar 
     if exten == '.pdf':
         r= requests.get(request,allow_redirects=True)
-        fileId = datetime.now().strftime("%S.%f")
         completeName= os.path.join('files', fileId+'.pdf')
         open(completeName,'wb').write(r.content)
          
--- core/views.py
+++ core/views.py
@@ -1,14 +1,36 @@
 from django.shortcuts import render
+from datetime import datetime
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 
+
 from core.steps import requestUrl
 from core.steps import summarizer
 
-# Create your views here.
+from models import AnalyticalReview
+
+# criação da lista de políticas de privacidade em análise
+analytical_review_list = []
+
+# criação da lista  de id de análises canceladas
+cancel_review_list = []
+
+# 
+# método POST para solicitar análise de política de privacidade
+# 
 @api_view(['POST', ])
 def request_url(request):
     if request.method == 'POST':
-        text = requestUrl.textExtractor(request.data['url'])
+
+        # criação do id para 
+        analytical_review = AnalyticalReview()
+        analytical_review.id = datetime.now().strftime("%S.%f")
+
+        # incluindo a análise na lista de políticas em análise
+        analytical_review_list.append(analytical_review)
+
+        # etapa de extração de texto bruto do PDF ou HTML
+        text = requestUrl.textExtractor(request.data['url'], analytical_review.id)
+
         return Response(summarizer.summarizerText(text))
