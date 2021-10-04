f089062d8688a72e9aaadd81f47ec7ecb622c7a0
Victor Marques <victyrmarques@gmail.com>
2021-10-04 20:46:14 -0300

    Ajuste nos comentários

--- core/views.py
+++ core/views.py
@@ -3,29 +3,28 @@ from datetime import datetime
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from core.models import AnalyticalReview
 from core.steps import requestUrl
 from core.steps import summarizer
 
-from core.models import AnalyticalReview
-
-# criação da lista de políticas de privacidade em análise
+# lista de políticas de privacidade em análise
 policies_under_analysis_review = []
 
 # 
-# método POST para solicitar análise de política de privacidade
+# método POST para análise de política de privacidade
 # 
 @api_view(['POST', ])
 def request_url(request):
     if request.method == 'POST':
 
-        # criação do id para 
+        # criação da esturutura e id da política de privacidade solicitada para análise
         policy_under_analysis = AnalyticalReview()
         policy_under_analysis.id = datetime.now().strftime("%S.%f")
 
         # incluindo a análise na lista de políticas em análise
         policies_under_analysis_review.append(policy_under_analysis)
 
-        #verifica se análise foi cancelada antes da próxima etapa 
+        # verifica se análise foi cancelada antes da próxima etapa 
         if(policy_under_analysis.cancel):
             cancel_policy_under_analysis(policy_under_analysis.id)
             return Response("")
@@ -33,7 +32,7 @@ def request_url(request):
         # etapa de extração de texto bruto do PDF ou HTML
         text = requestUrl.textExtractor(request.data['url'], policies_under_analysis_review.id)
 
-        #verifica se análise foi cancelada antes da próxima etapa 
+        # verifica se análise foi cancelada antes da próxima etapa 
         if(policy_under_analysis.cancel):
             cancel_policy_under_analysis(policy_under_analysis.id)
             return Response("")
@@ -47,7 +46,7 @@ def cancel_policy_under_analysis(review_id):
     # recupera index pelo id informado
     policy_index = next((p for p in policies_under_analysis_review if p.id == review_id), -1)
 
-    # verfica se o id informado existe
+    # verifica se o id informado existe
     if(policy_index != -1):
         # caso sim, remove o arquivo criado e da lista de políticas em análise
         requestUrl.removeFile(review_id)
