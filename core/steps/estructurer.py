6a9f6c81032961e4002d0011c4869bfeda3d5dc3
VictorMatheus819 <victormatheus819@gmail.com>
2021-10-06 20:47:43 -0300

    Adição de  flag de texto generico

--- core/steps/estructurer.py
+++ core/steps/estructurer.py
@@ -1,13 +1,33 @@
 
 import re
+import json
 
 
 def Sinalize (request):
-    subwords=['CPF',"EMAIL","TELEFONE", "SENHA","CELULAR","SEXO","ENDEREÇO","EMAIL","CNPJ","NOME","FINALIDADE","DADOS","FINS"]
+    payload ={
+        'text':'',
+        'flag':False
+        }
+    subwords=['CPF',"EMAIL","E-MAIL","TELEFONE", "SENHA","CELULAR","SEXO","ENDEREÇO","EMAIL","CNPJ","NOME","FINALIDADE","DADOS","FINS"]
     text=request
     for word in subwords:
         compiled = re.compile(re.escape(word), re.IGNORECASE)
         text = compiled.sub("<b>" + word + "</b>",  text)
     
-    text=text.replace("\n","<br>")    
-    return text
+    text=text.replace("\n","<br>") 
+    payload['text']= text
+    
+    especificdata=['CPF',"EMAIL","E-MAIL","TELEFONE", "SENHA","CELULAR","SEXO","ENDEREÇO","EMAIL","CNPJ","NOME"] 
+    
+    points=0
+    for data in especificdata: 
+        if re.findall(data,text):
+            points= points+1
+    if points==0:
+        payload['flag']= True
+    else:
+          payload['flag']= False  
+    
+     
+    
+    return payload
