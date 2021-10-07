29161d550e02ca5884f3d4aecb8e76421cebf07c
VictorMatheus819 <victormatheus819@gmail.com>
2021-10-06 21:11:13 -0300

    Adição de verificação de presença de parametro

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from datetime import datetime
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 from core.models import AnalyticalReview
 from core.steps import requestUrl
@@ -15,33 +16,38 @@ policies_under_analysis_review = []
 # 
 @api_view(['POST', ])
 def start_analysis(request):
-    if request.method == 'POST':
+    # Verifica se  contem o parametro "url" no corpo da requisição
+    if "url" in request.data:
+        if request.method == 'POST':
 
-        # criação da esturutura e id da política de privacidade solicitada para análise
-        policy_under_analysis = AnalyticalReview()
-        policy_under_analysis.id = datetime.now().strftime("%S.%f")
+            # criação da esturutura e id da política de privacidade solicitada para análise
+            policy_under_analysis = AnalyticalReview()
+            policy_under_analysis.id = datetime.now().strftime("%S.%f")
 
-        # incluindo a análise na lista de políticas em análise
-        policies_under_analysis_review.append(policy_under_analysis)
+            # incluindo a análise na lista de políticas em análise
+            policies_under_analysis_review.append(policy_under_analysis)
 
-        # verifica se análise foi cancelada antes da próxima etapa 
-        if(policy_under_analysis.cancel):
-            cancel_policy_under_analysis(policy_under_analysis.id)
-            return Response("")
+            # verifica se análise foi cancelada antes da próxima etapa 
+            if(policy_under_analysis.cancel):
+                cancel_policy_under_analysis(policy_under_analysis.id)
+                return Response("")
 
-        # etapa de extração de texto bruto do PDF ou HTML
-        text = requestUrl.textExtractor(request.data['url'], policy_under_analysis.id)
+            # etapa de extração de texto bruto do PDF ou HTML
+            text = requestUrl.textExtractor(request.data['url'], policy_under_analysis.id)
 
-        # verifica se análise foi cancelada antes da próxima etapa 
-        if(policy_under_analysis.cancel):
-            cancel_policy_under_analysis(policy_under_analysis.id)
-            return Response("")
+            # verifica se análise foi cancelada antes da próxima etapa 
+            if(policy_under_analysis.cancel):
+                cancel_policy_under_analysis(policy_under_analysis.id)
+                return Response("")
 
-        # etapa de sumarização do texto bruto
-        text = summarizer.summarizer_text(text)
+            # etapa de sumarização do texto bruto
+            text = summarizer.summarizer_text(text)
 
-        return Response(text)
-
+            return Response(text)
+    else:
+        data={}
+        data["error"]="Falta do parametro url no corpo da requisição"
+        return Response(data=data,status=status.HTTP_400_BAD_REQUEST)
 #
 # método para cancelar política de privacidade em análise em processamento
 #
@@ -60,15 +66,21 @@ def cancel_policy_under_analysis(review_id):
 # 
 @api_view(['POST', ])
 def cancel_analysis(request):
-    if request.method == 'POST':
+    # Verifica se  contem o parametro "id" no corpo da requisição
+    if "id" in request.data:
+        if request.method == 'POST':
 
-        # recupera index pelo id informado
-        policy_index = next((p for p in policies_under_analysis_review if p.id == request.data['id']), -1)
+            # recupera index pelo id informado
+            policy_index = next((p for p in policies_under_analysis_review if p.id == request.data['id']), -1)
 
-        # verifica se o id informado existe
-        if(policy_index != -1):
-            # caso sim, seta como cancelado na lista de políticas de privacidade em análise
-            policies_under_analysis_review[policy_index].cancel = True
+            # verifica se o id informado existe
+            if(policy_index != -1):
+                # caso sim, seta como cancelado na lista de políticas de privacidade em análise
+                policies_under_analysis_review[policy_index].cancel = True
 
-        # TODO verificar o retorno
-        return Response(True)
+            # TODO verificar o retorno
+            return Response(True)
+    else:
+        data={}
+        data["error"]="Falta do parametro id no corpo da requisição"
+        return Response(data=data,status=status.HTTP_400_BAD_REQUEST)       
