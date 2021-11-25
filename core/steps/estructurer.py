17f7aca6e2c2028c1d6fcde75d2ce60094cfdbda
VictorMatheus819 <victormatheus819@gmail.com>
2021-11-25 19:44:27 -0300

    mudança na cor da sinalização

--- core/steps/estructurer.py
+++ core/steps/estructurer.py
@@ -9,14 +9,14 @@ def sinalize(request):
     # Sinalização dos dados que serão coletados no texto sumarizado focado nas informações de coleta 
     for word in especific_data:
         compiled = re.compile(re.escape(" " + word), re.IGNORECASE)
-        request["coleta"] = compiled.sub("<b>" + " " + word + "</b>",  request["coleta"])
+        request["coleta"] = compiled.sub("<b class='sinalizer'>" + " " + word + "</b>",  request["coleta"])
     
     request["coleta"] = request["coleta"].replace("\n","<br>") 
     
      # Sinalização dos dados que serão coletados no texto sumarizado focado nas finalidades de uso de dados
     for word in especific_data:
         compiled = re.compile(re.escape(" " + word), re.IGNORECASE)
-        request["finalidade"] = compiled.sub("<b>" + " " + word + "</b>", request["finalidade"])
+        request["finalidade"] = compiled.sub("<b class='sinalizer'>" + " " + word + "</b>", request["finalidade"])
 
     request["finalidade"] = request ["finalidade"].replace("\n","<br>") 
     
